5befb276f34ab547ef3317647144db87a817dbe4
Semih <semih.turkoglu@student.vinci.be>
2023-10-16 14:06:25 +0200

    2.3.2 improvement

--- exercices/2.3.2/index.js
+++ exercices/2.3.2/index.js
@@ -1,13 +1,22 @@
 const divs = document.querySelectorAll('div');
 divs.forEach((div) => {
+    let big = false;
   div.addEventListener('click', (e) => {
-    e.target.innerText = e.target.style.backgroundColor;
+    if (!big) {
+e.target.innerText = e.target.style.backgroundColor;
     e.target.style.width = "100px";
     e.target.style.height = "100px";
-  });
-  div.addEventListener('click', (e) => {
+    big = true;
+  }
+     else {
+        div.addEventListener('click', (e) => {
     e.target.innerText="";
-    e.target.style.width="5Opx";
+    e.target.style.width="50px";
     e.target.style.height="50px";
-  })
-});
+    e.target.display= "inline-block";
+    big = false;
+  });
+}
+    
+  
+})});
